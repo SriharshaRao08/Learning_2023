2afadab3d71cfc52891809e9ce4d7176040eb5be
SriharshaRao08 <raosriharsha08@gmail.com>
2023-06-14 19:19:33 +0530

    L1 example3 code using loops

--- Module1/Day2/Example3.c
+++ Module1/Day2/Example3.c
@@ -4,7 +4,7 @@ int main() {
     int arr[500] ;
     int size = sizeof(arr) / sizeof(arr[0]);
     int a,sum = 0;
-    printf("Enter no. of integers ");
+    printf("Enter the size ");
     scanf("%d",&a);
 
   
@@ -16,8 +16,30 @@ int main() {
         sum += arr[i];
     }
     
-    printf("Sum of alternate elements: %d\n", sum);
+    printf("Sum = %d\n", sum);
 
     return 0;
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+int main() {
+    int size, num, sum = 0;
+
+    printf("Enter the size ");
+    scanf("%d", &size);
+
+
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &num);
+
+        if (i % 2 == 0) {
+            sum += num;
+        }
+    }
+
+    printf("Sum = %d\n", sum);
+
+    return 0;
+}
